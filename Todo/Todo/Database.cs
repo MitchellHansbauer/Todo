8d80694cd5fc14020e3f6b1bbb08c96d8fc67ea7
MitchellHansbauer <hansbama@mail.uc.edu>
2024-03-10 15:10:25 -0400

    Add project files.

---
+++ Todo/Todo/Database.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Todo
+{
+    public class Database
+    {
+        private readonly SQLiteAsyncConnection database;
+        public Database(string dbPath)
+        {
+            database = new SQLiteAsyncConnection(dbPath);
+            database.CreateTableAsync<TodoTask>();
+        }
+
+        public Task<List<TodoTask>> GetToDoAsync()
+        {
+            return database.Table<TodoTask>().ToListAsync();
+        }
+
+        public Task<int> SaveTodoTaskAsync(TodoTask todoTask)
+        {
+            return database.InsertAsync(todoTask);
+        }
+
+
+    }
+}
---
+++ Todo/Todo/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace Todo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            LoadTasks();
+        }
+
+        public ObservableCollection<TodoTask> TodoTasks { get; set; }
+        public TodoTask SelectedTask { get; set; }
+
+        async void LoadTasks()
+        {
+            TodoTasks = new ObservableCollection<TodoTask>(await App.Database.GetToDoAsync());
+        }
+
+        async void OnSaveTaskClicked(object sender, EventArgs e)
+        {
+            var todoTask = new TodoTask
+            {
+                Name = taskNameEntry.Text,
+                Notes = notesEditor.Text,
+                Done = false // Default value for Done
+            };
+
+            await App.Database.SaveTodoTaskAsync(todoTask);
+            TodoTasks.Add(todoTask);
+
+            taskNameEntry.Text = string.Empty;
+            notesEditor.Text = string.Empty;
+
+            await DisplayAlert("Success", "Task saved successfully", "OK");
+        }
+
+        async void OnMarkAsCompletedClicked(object sender, EventArgs e)
+        {
+            if (SelectedTask != null)
+            {
+                SelectedTask.Done = true;
+                await App.Database.SaveTodoTaskAsync(SelectedTask);
+                await DisplayAlert("Success", "Task marked as completed", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Error", "Please select a task", "OK");
+            }
+        }
+    }
+}
